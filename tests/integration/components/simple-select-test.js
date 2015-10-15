b1b67820e26213093b003e9bc831cbb87c45feba
GreatWizard <guillaume.gerard88@gmail.com>
2015-10-15 12:26:14 +0200

    Selection by key

--- tests/integration/components/simple-select-test.js
+++ tests/integration/components/simple-select-test.js
@@ -6,6 +6,8 @@ moduleForComponent('simple-select', 'Integration | Component | simple select', {
 });
 
 test('it can be disabled', function(assert) {
+  assert.expect(3);
+
   this.render(hbs`{{simple-select}}`);
   assert.ok(this.$('select').not(':disabled'), 'Enabled by default');
 
@@ -15,6 +17,32 @@ test('it can be disabled', function(assert) {
 
   this.set('testDisabled', true);
   this.render(hbs`{{simple-select disabled=testDisabled}}`);
-
   assert.ok(this.$('select').is(':disabled'), 'Explicitly disabled');
+});
+
+test('it initializes with the selected object', function(assert) {
+  assert.expect(1);
+
+  this.set('content', [
+    { id: 1, title: 'One'},
+    { id: 2, title: 'Two'}
+  ]);
+  this.set('selected', { id: 3, title: 'Three'});
+  this.get('content').push(this.get('selected'));
+
+  this.render(hbs`{{simple-select content=content selection=selected}}`);
+  assert.equal(this.$('select').val(), 3);
+});
+
+test('it initializes with the selected value', function(assert) {
+  assert.expect(1);
+
+  this.set('content', [
+    { id: 1, title: 'One'},
+    { id: 2, title: 'Two'},
+    { id: 3, title: 'Three'}
+  ]);
+
+  this.render(hbs`{{simple-select content=content value=3}}`);
+  assert.equal(this.$('select').val(), 3);
 });
