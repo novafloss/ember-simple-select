a86208766a6bcd52ec065a1c1e3cd3e5517d7926
Jeldrik Hanschke <admin@jhanschke.de>
2018-12-29 23:40:12 +0100

    upgrade to support ember 3.4

--- addon/components/simple-select.js
+++ addon/components/simple-select.js
@@ -1,10 +1,14 @@
 //
 // Shamelessly copied from http://jsbin.com/fotuqa
 //
-import Ember from 'ember';
+import { A } from '@ember/array';
+
+import { computed, get } from '@ember/object';
+import { not } from '@ember/object/computed';
+import Component from '@ember/component';
 import layout from '../templates/components/simple-select';
 
-export default Ember.Component.extend({
+export default Component.extend({
   layout,
   tagName: 'select',
   attributeBindings: ['disabled'],
@@ -14,14 +18,14 @@ export default Ember.Component.extend({
   optionValuePath: 'id',
   optionLabelPath: 'title',
   allowEmpty: false,
-  disablePrompt: Ember.computed.not('allowEmpty').readOnly(),
+  disablePrompt: not('allowEmpty').readOnly(),
 
-  _selection: Ember.computed('selection', 'value', 'content', 'optionValuePath', {
+  _selection: computed('selection', 'value', 'content', 'optionValuePath', {
     get() {
       const selection = this.get('selection');
       let _sel = null;
       if (!selection && this.get('value')) {
-        _sel = Ember.A(this.get('content')).findBy(this.get('optionValuePath'), this.get('value'));
+        _sel = A(this.get('content')).findBy(this.get('optionValuePath'), this.get('value'));
       } else if (selection) {
         _sel = selection;
       }
@@ -37,7 +41,7 @@ export default Ember.Component.extend({
   },
 
   change() {
-    const [ { selectedIndex } ] = this.$().toArray();
+    const { selectedIndex } = this.element;
     if (selectedIndex === -1) {
       return;
     }
@@ -46,8 +50,8 @@ export default Ember.Component.extend({
     const hasPrompt = !!this.get('prompt');
     const contentIndex = hasPrompt ? selectedIndex - 1 : selectedIndex;
 
-    const selection = Ember.A(this.get('content')).objectAt(contentIndex);
-    const value = selection ? Ember.get(selection, this.get('optionValuePath')) : null;
+    const selection = A(this.get('content')).objectAt(contentIndex);
+    const value = selection ? get(selection, this.get('optionValuePath')) : null;
 
     this.sendAction('action', selection, value);
   }
--- tests/integration/components/simple-select-test.js
+++ tests/integration/components/simple-select-test.js
@@ -1,163 +1,173 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import Ember from 'ember';
+import { A } from '@ember/array';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, find, findAll, fillIn, triggerEvent } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('simple-select', 'Integration | Component | simple select', {
-  integration: true
-});
+module('Integration | Component | simple select', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it can be disabled', function(assert) {
-  assert.expect(3);
+  hooks.beforeEach(function() {
+    this.actions = {};
+    this.send = (actionName, ...args) => this.actions[actionName].apply(this, args);
+  });
 
-  this.render(hbs`{{simple-select}}`);
-  assert.ok(this.$('select').not(':disabled'), 'Enabled by default');
+  test('it can be disabled', async function(assert) {
+    assert.expect(3);
 
-  this.set('testDisabled', false);
-  this.render(hbs`{{simple-select disabled=testDisabled}}`);
-  assert.ok(this.$('select').not(':disabled'), 'Explicitly enabled');
+    await render(hbs`{{simple-select}}`);
+    assert.notOk(find('select').disabled, 'Enabled by default');
 
-  this.set('testDisabled', true);
-  this.render(hbs`{{simple-select disabled=testDisabled}}`);
-  assert.ok(this.$('select').is(':disabled'), 'Explicitly disabled');
-});
+    this.set('testDisabled', false);
+    await render(hbs`{{simple-select disabled=testDisabled}}`);
+    assert.notOk(find('select').disabled, 'Explicitly enabled');
 
-test('it can be an empty list', function(assert) {
-  assert.expect(2);
+    this.set('testDisabled', true);
+    await render(hbs`{{simple-select disabled=testDisabled}}`);
+    assert.ok(find('select').disabled, 'Explicitly disabled');
+  });
 
-  this.render(hbs`{{simple-select}}`);
-  assert.equal(this.$('select > option').length, 0, 'No content, no placeholder');
+  test('it can be an empty list', async function(assert) {
+    assert.expect(2);
 
-  this.render(hbs`{{simple-select prompt="hey"}}`);
-  assert.equal(this.$('select > option').length, 1, 'No content, but a prompt is present');
-});
+    await render(hbs`{{simple-select}}`);
+    assert.equal(findAll('select > option').length, 0, 'No content, no placeholder');
 
-test('it initializes with the selected object', function(assert) {
-  assert.expect(1);
+    await render(hbs`{{simple-select prompt="hey"}}`);
+    assert.equal(findAll('select > option').length, 1, 'No content, but a prompt is present');
+  });
 
-  this.set('content', [
-    { id: 1, title: 'One'},
-    { id: 2, title: 'Two'}
-  ]);
-  this.set('selected', { id: 3, title: 'Three'});
-  this.get('content').push(this.get('selected'));
+  test('it initializes with the selected object', async function(assert) {
+    assert.expect(1);
 
-  this.render(hbs`{{simple-select content=content selection=selected}}`);
-  assert.equal(this.$('select').val(), 3);
-});
+    this.set('content', [
+      { id: 1, title: 'One'},
+      { id: 2, title: 'Two'}
+    ]);
+    this.set('selected', { id: 3, title: 'Three'});
+    this.get('content').push(this.get('selected'));
 
-test('it initializes with the selected value', function(assert) {
-  assert.expect(1);
+    await render(hbs`{{simple-select content=content selection=selected}}`);
+    assert.equal(find('select').value, 3);
+  });
 
-  this.set('content', [
-    { id: 1, title: 'One'},
-    { id: 2, title: 'Two'},
-    { id: 3, title: 'Three'}
-  ]);
+  test('it initializes with the selected value', async function(assert) {
+    assert.expect(1);
 
-  this.render(hbs`{{simple-select content=content value=3}}`);
-  assert.equal(this.$('select').val(), 3);
-});
+    this.set('content', [
+      { id: 1, title: 'One'},
+      { id: 2, title: 'Two'},
+      { id: 3, title: 'Three'}
+    ]);
 
-test('It changes the selected option when the "selection" attribute is updated', function(assert) {
-  assert.expect(2);
+    await render(hbs`{{simple-select content=content value=3}}`);
+    assert.equal(find('select').value, 3);
+  });
 
-  this.set('content', [
-    { id: 1, title: 'One'},
-    { id: 2, title: 'Two'},
-    { id: 3, title: 'Three'}
-  ]);
+  test('It changes the selected option when the "selection" attribute is updated', async function(assert) {
+    assert.expect(2);
 
-  this.set('selection', this.get('content.2'));
-  this.render(hbs`{{simple-select content=content selection=selection}}`);
-  assert.equal(this.$('select').val(), 3);
-  this.set('selection', this.get('content.0'));
-  assert.equal(this.$('select').val(), 1);
-});
+    this.set('content', [
+      { id: 1, title: 'One'},
+      { id: 2, title: 'Two'},
+      { id: 3, title: 'Three'}
+    ]);
 
-test('It changes the selected option when the "selection" is updated by user click', function(assert) {
-  assert.expect(2);
+    this.set('selection', this.get('content.2'));
+    await render(hbs`{{simple-select content=content selection=selection}}`);
+    assert.equal(find('select').value, 3);
+    this.set('selection', this.get('content.0'));
+    assert.equal(find('select').value, 1);
+  });
 
-  this.set('content', [
-    { id: 1, title: 'One'},
-    { id: 2, title: 'Two'},
-    { id: 3, title: 'Three'}
-  ]);
+  test('It changes the selected option when the "selection" is updated by user click', async function(assert) {
+    assert.expect(2);
 
-  this.set('selection', this.get('content.2'));
-  this.render(hbs`{{simple-select content=content selection=selection}}`);
-  assert.equal(this.$('select').val(), 3);
-  this.$('select option:nth-child(2)').attr('selected', 'selected').trigger('change');
-  assert.equal(this.$('select').val(), 2);
-});
+    this.set('content', [
+      { id: 1, title: 'One'},
+      { id: 2, title: 'Two'},
+      { id: 3, title: 'Three'}
+    ]);
 
-test('it triggers an action on change', function(assert) {
-  assert.expect(1);
+    this.set('selection', this.get('content.2'));
+    await render(hbs`{{simple-select content=content selection=selection}}`);
+    assert.equal(find('select').value, 3);
 
-  this.set('content', [
-    { id: 1, title: 'One'},
-    { id: 2, title: 'Two'},
-    { id: 3, title: 'Three'}
-  ]);
+    let target = find('select option:nth-child(2)');
+    target.selected = true;
+    await triggerEvent(target, 'change');
+    assert.equal(find('select').value, 2);
+  });
 
-  this.didChange= ()=> assert.ok(true);
-  this.on('didChange', this.didChange);
-  this.render(hbs`{{simple-select content=content action='didChange'}}`);
-  this.$('select').val(2).trigger('change');
-});
+  test('it triggers an action on change', async function(assert) {
+    assert.expect(1);
 
-test('it sends the selected object and the value as a parameter', function(assert) {
-  assert.expect(2);
+    this.set('content', [
+      { id: 1, title: 'One'},
+      { id: 2, title: 'Two'},
+      { id: 3, title: 'Three'}
+    ]);
 
-  this.set('content', [
-    { id: 1, title: 'One'},
-    { id: 2, title: 'Two'},
-    { id: 3, title: 'Three'}
-  ]);
+    this.didChange= ()=> assert.ok(true);
+    this.actions.didChange = this.didChange;
+    await render(hbs`{{simple-select content=content action='didChange'}}`);
+    await fillIn('select', 2);
+  });
 
-  this.didChange = (selection, value)=> {
-    assert.equal(selection, Ember.A(this.get('content')).objectAt(2));
-    assert.equal(value, 3);
-  };
-  this.on('didChange', this.didChange);
-  this.render(hbs`{{simple-select content=content action='didChange'}}`);
-  this.$('select').val(3).trigger('change');
-});
+  test('it sends the selected object and the value as a parameter', async function(assert) {
+    assert.expect(2);
 
-test('it allows a prompt', function(assert) {
-  assert.expect(2);
+    this.set('content', [
+      { id: 1, title: 'One'},
+      { id: 2, title: 'Two'},
+      { id: 3, title: 'Three'}
+    ]);
 
-  this.set('content', [
-    { id: 3, title: 'Three'}
-  ]);
+    this.didChange = (selection, value)=> {
+      assert.equal(selection, A(this.get('content')).objectAt(2));
+      assert.equal(value, 3);
+    };
+    this.actions.didChange = this.didChange;
+    await render(hbs`{{simple-select content=content action='didChange'}}`);
+    await fillIn('select', 3);
+  });
 
-  this.render(hbs`{{simple-select content=content prompt='Hep'}}`);
-  assert.ok(this.$('select').text().match('Hep'), 'prompt is selected by default');
-  assert.ok(this.$('option').is(':disabled'), 'prompt is disabled by default');
-});
+  test('it allows a prompt', async function(assert) {
+    assert.expect(2);
 
-test('it has a configurable prompt', function(assert) {
-  assert.expect(1);
+    this.set('content', [
+      { id: 3, title: 'Three'}
+    ]);
 
-  this.set('content', [
-    { id: 3, title: 'Three'}
-  ]);
+    await render(hbs`{{simple-select content=content prompt='Hep'}}`);
+    assert.ok(find('select').textContent.match('Hep'), 'prompt is selected by default');
+    assert.ok(find('option').disabled, 'prompt is disabled by default');
+  });
 
-  this.render(hbs`{{simple-select content=content prompt='Hep' allowEmpty=true}}`);
-  assert.ok(this.$('option').not(':disabled'), 'prompt can be enabled');
-});
+  test('it has a configurable prompt', async function(assert) {
+    assert.expect(1);
 
-test('it can allow an empty selection when the prompt is selectable', function(assert) {
-  assert.expect(2);
+    this.set('content', [
+      { id: 3, title: 'Three'}
+    ]);
 
-  this.set('content', [
-    { id: 3, title: 'Three'}
-  ]);
+    await render(hbs`{{simple-select content=content prompt='Hep' allowEmpty=true}}`);
+    assert.notOk(find('option').disabled, 'prompt can be enabled');
+  });
 
-  this.render(hbs`{{simple-select content=content value=3 prompt='Hep' allowEmpty=true action='didChange'}}`);
-  this.didChange = (sel, v)=> {
-    assert.equal(sel, null);
-    assert.equal(v, null);
-  };
-  this.on('didChange', this.didChange);
-  this.$('select').val(undefined).trigger('change');
+  test('it can allow an empty selection when the prompt is selectable', async function(assert) {
+    assert.expect(2);
+
+    this.set('content', [
+      { id: 3, title: 'Three'}
+    ]);
+
+    await render(hbs`{{simple-select content=content value=3 prompt='Hep' allowEmpty=true action='didChange'}}`);
+    this.didChange = (sel, v)=> {
+      assert.equal(sel, null);
+      assert.equal(v, null);
+    };
+    this.actions.didChange = this.didChange;
+    await fillIn('select', '');
+  });
 });
